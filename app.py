dedf881162d8719fde7529cb137d4067ff87456c
Falbue <cyansair05@gmail.com>
2024-10-30 16:30:17 +0500

    Добавлены файлы в передачу

--- app.py
+++ app.py
@@ -2,6 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.context_processor
+def inject_files():
+    css_directory = os.path.join(app.static_folder, 'styles')
+    js_directory = os.path.join(app.static_folder, 'scripts')
+    
+    css_files = get_files(css_directory, ['.css'])
+    js_files = get_files(js_directory, ['.js'])
+    
+    return dict(css_files=css_files, js_files=js_files)
+
 @app.route('/')
 def index():
     return render_template('index.html')
