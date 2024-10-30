433901ecaf9848b6f987218bec44cca760fbefd7
Falbue <cyansair05@gmail.com>
2024-10-30 16:07:18 +0500

    убрал лишнее

--- app.py
+++ app.py
@@ -1,14 +1,5 @@
 # app.py
 from flask import Flask, render_template
-import atexit
-import os
-import signal
-
-def cleanup():
-    pid = os.getpid()
-    os.kill(pid, signal.SIGTERM)
-
-atexit.register(cleanup)
 
 app = Flask(__name__)
 
