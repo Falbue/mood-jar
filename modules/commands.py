9f8c625cb8f10f6ea8eb80ea40880e93593f0c1c
Falbue <cyansair05@gmail.com>
2024-10-30 21:28:03 +0500

    улучшение кода

--- modules/commands.py
+++ modules/commands.py
@@ -5,18 +5,18 @@ def registration(message):
     message_id = message.message_id
     username = message.from_user.username
 
-    times = now_time()
+    date, time  = now_time()
     user = SQL_request("SELECT 0 FROM users WHERE id = ?", (user_id,))
     if user is None:
         SQL_request("""INSERT INTO users (id, message, time_registration)
-                          VALUES (?, ?, ?)""", (user_id, message_id+1, times))
+                          VALUES (?, ?, ?)""", (user_id, message_id+1, date))
         print(f"Зарегистрирован новый пользователь")
     else:
         menu_id = SQL_request("SELECT message FROM users WHERE id = ?", (user_id,))
         SQL_request("""UPDATE users SET message = ?, username = ? WHERE id = ?""", (message_id+1, username, user_id))  # добавление id нового меню
         return menu_id
 
-def profile(user):
+def info(user):
     user_id = user[0]
     frend = user[2]
     if frend is None:
