073e661a7667f1963c95e724db87406962bbbb84
Falbue <cyansair05@gmail.com>
2024-10-30 16:19:58 +0500

    Разложил всё по папкам

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# app.py
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,5 +6,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+if __name__ == "__main__":
+    app.run(debug=True)
